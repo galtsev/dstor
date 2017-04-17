0a1c4b8a22877ffb83732cf116ccaed738131780
Denis Galtsev <galtsevdv@gmail.com>
2017-04-17 11:32:59 +0300

    cleanup - remove receptors

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"dan/pimco/command"
-	"dan/pimco/receptor/batch"
-	"dan/pimco/receptor/direct"
-	"dan/pimco/receptor/kafka"
 	"fmt"
 	"log"
 	"os"
@@ -15,18 +12,8 @@ func Usage() {
 	fmt.Printf(`Usage: %s <cmd> <options>
 commands: 
 	client - test http client, 
-	recept-direct - http server that write samples directly to influxdb,
-	recept-kafka - http server that write samples to kafka using async producer (one sample per kafka message)
-	recept-batch - http server that write samples to kafka (many samples per kafka message)
-	loader - pump messages from kafka to influxdb
-
-	# Reporting servers
 	reporting-server-mem - reporting server with embedded loader. Read Kafka continuously, keep all samples in memory.
-
 	show-config - dump current config to stdout
-
-	# experimenting
-	gen1 - ?
 	gen2flux - generate samples and write directly to influxdb
 	gen2kafka - generate samples and write to kafka
 	kafka2flux - read from kafka, write to influxdb
@@ -54,14 +41,6 @@ func main() {
 		timeIt(func() {
 			command.Client(args)
 		})
-	case "recept-direct":
-		direct.Run(args)
-	case "recept-kafka":
-		kafka.Run(args)
-	case "recept-batch":
-		batch.Run(args)
-
-	// temporary
 	case "query-flux":
 		timeIt(func() { command.QueryFlux(args) })
 		//command.QueryFlux(args)
