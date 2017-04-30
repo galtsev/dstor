0911b3647ceb366fe5f2b3ddcf83726e85d5fd78
Denis Galtsev <galtsevdv@gmail.com>
2017-04-30 18:33:25 +0300

    remote reporter

---
+++ ldb/remote_reporter.go
@@ -0,0 +1,37 @@
+package ldb
+
+import (
+	"dan/pimco"
+	"dan/pimco/api"
+	"dan/pimco/conf"
+	"time"
+)
+
+type Reporter struct {
+	pimco.FakeStorage
+	nodes       []*api.Client
+	partitioner func(string) int32
+}
+
+func NewReporter(cfg conf.Config) *Reporter {
+	cluster := Reporter{
+		partitioner: pimco.MakePartitioner(cfg.Kafka.NumPartitions),
+	}
+	// TODO: get nodes from Zookeeper
+	for p := 0; p < cfg.Kafka.NumPartitions; p++ {
+		client := api.NewClient(cfg.Client)
+		cluster.nodes = append(cluster.nodes, client)
+	}
+	return &cluster
+}
+
+func (srv *Reporter) Report(tag string, start, stop time.Time) []pimco.ReportLine {
+	partition := srv.partitioner(tag)
+	return srv.nodes[partition].Report(tag, start, stop)
+}
+
+func init() {
+	pimco.RegisterBackend("remote", func(cfg conf.Config) pimco.Backend {
+		return NewReporter(cfg)
+	})
+}
